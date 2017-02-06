142b6482875b4da8bcaddc01c94aa50b13ed6045
Aaron Vanston <aaronvanston@users.noreply.github.com>
2017-02-06 15:07:30 +1100

    Aside nav styling (#8)
    * feat: add clear-list mixin
    
    * refactor: update heading font to alt
    
    * refactor: update heading font to alt
    
    * feat: add new menu styls and structure
    
    * fix: key index

--- src/common/components/aside/Aside.js
+++ src/common/components/aside/Aside.js
@@ -1,19 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 import { Header } from '~/common/components';
 
 import styles from './Aside.css';
 
+const mainMenuLinks = [{
+  title: 'Properties',
+  url: '/properties',
+}, {
+  title: 'Selectors',
+  url: '/selctors',
+}, {
+  title: 'Groups',
+  url: '/groups',
+}, {
+  title: 'Cheatsheets',
+  url: '/cheatsheets',
+}, {
+  seperator: true,
+}, {
+  title: 'Suggest and edit',
+  url: '/suggest-an-edit',
+}, {
+  title: 'Add a page',
+  url: '/add-a-page',
+}];
+
+
 const Aside = () => (
   <aside className={styles.aside}>
     <Header />
 
-    <nav>
-      <ul>
-        <li>Properties</li>
-        <li>Selectors</li>
-        <li>Groups</li>
-        <li>Cheatsheet</li>
+    <nav className={styles.nav}>
+      <ul className={styles.navGroup}>
+        {mainMenuLinks.map((link, linkIndex) => (
+          (link.seperator) ?
+            <div key={linkIndex} className={styles.linkSeperator} /> :
+            <li key={linkIndex} className={styles[link.title.toLowerCase()]}>
+              <Link
+                className={styles.navLink}
+                activeClassName={styles.navLinkActive}
+                to={link.url}
+              >
+                {link.title}
+              </Link>
+            </li>
+        ))}
       </ul>
     </nav>
   </aside>
