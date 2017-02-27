bbfad0c7dc88019e2876f3ec78b873cfb0740e3f
Aaron Vanston <aaron.vanston@rea-group.com>
2017-02-27 22:12:38 +1100

    refactor: update links to slugs

--- src/common/components/sectionIndex/SectionIndex.js
+++ src/common/components/sectionIndex/SectionIndex.js
@@ -1,22 +1,25 @@
 import React, { PropTypes } from 'react';
 import _ from 'lodash';
 import { Link } from 'react-router';
+import fm from 'front-matter';
 
 import * as content from '@/';
 
 const SectionIndex = (props) => {
   const section = props.params.section;
   const articles = _(content[section]).toPairs().sortBy(0).value();
-
   return (
     <div>
       <h1>Section: {section}</h1>
       {content[section] ?
-        articles.map((article, articleIndex) => (
-          <div key={articleIndex}>
-            <Link to={`${section}/${article[0]}`}>{article[0]}</Link>
-          </div>
-        ))
+        articles.map((article, articleIndex) => {
+          const slug = fm(article[1]).attributes.title;
+          return (
+            <div key={articleIndex}>
+              <Link to={`${section}/${slug}`}>{slug}</Link>
+            </div>
+          );
+        })
         : <div>Like there is no content here for {section}</div>
       }
     </div>
